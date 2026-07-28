c6d4eb84efd614920bf2a7298d39a0a23ee162c2
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2026-07-28 12:01:14 +0530

    hasing list & dict

---
+++ 04_hashing.py
@@ -0,0 +1,54 @@
+# nums = [1,2,3,4,5,6,7,8,5,9,10]
+# hashmap=dict()
+# n= len(nums)
+# for i in range(0,n):
+#     hashmap[nums[i]] = hashmap.get(nums[i],0)+1
+# print(hashmap[5])
+
+
+
+
+
+# n = [1,2,4,4,5,6,9,7,8,7,8,2,2,3,1,1,9,10,10,9,10]
+# m = [2,23,32,1,9,7,5,3,10,111] 
+
+# hash_list = [0]* 11
+# for num in n:
+#     hash_list[num] += 1 
+# print(hash_list)
+# res=[]
+# for num in m:
+#     if num>10 or num<1:
+#         res.append(0)
+#     else:
+#         res.append(hash_list[num])
+# print(res)
+
+
+
+nums = [1,2,4,4,5,6,9,7,8,7,8,2,2,3,1,1,9,10,10,9,10]
+m = [2,23,32,1,9,7,5,3,10,111] 
+hash_map = {}
+size = len(nums)
+for i in range(0,size):
+    hash_map[nums[i]] = hash_map.get(nums[i],0)+1
+res_dict = {}
+for i in range(0,len(m)):
+    res_dict[m[i]] = hash_map.get(m[i],0)
+print(res_dict)
+print(hash_map)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
