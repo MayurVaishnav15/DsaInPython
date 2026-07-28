88b8e91d2517dd4855e0e9737beffa18e8bc0a98
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2026-07-28 16:33:44 +0530

    factorial using rec

--- recursion/factorialOfNumber.py
+++ recursion/factorialOfNumber.py
@@ -1,7 +1,30 @@
-def factorial(num):
-    if(num==0 or num==1):
-        return 1
-    else:
-        return num*factorial(num-1)
+# def factorial(num):
+#     if(num==0 or num==1):
+#         return 1
+#     else:
+#         return num*factorial(num-1)
 
-print(factorial(5))
+# print(factorial(5))
+
+
+
+
+
+def factorial(n):
+    if n==1:
+        return 1
+    return n*factorial(n-1)
+n=4
+print(f"Factorial of {n} is",factorial(n))
+
+
+
+
+
+
+
+
+
+
+
+
