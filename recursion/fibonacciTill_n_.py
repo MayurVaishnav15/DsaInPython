a38b2d83a00b8f27f910c4f096ba300e1639c4ba
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2026-07-30 18:17:05 +0530

    fibo series

--- recursion/fibonacciTill_n_.py
+++ recursion/fibonacciTill_n_.py
@@ -1,4 +1,4 @@
-# This is normal approach 2^n both 
+# # This is normal approach 2^n both 
 # def fib(n):
 #     a,b=0,1
 #     print(a,b,end=" ")
@@ -9,12 +9,16 @@
 #         b=c
 # fib(9)
 
-# This is rec TC and SC = O(2^n) 
-def fib(n):
-    if n==0:
-        return 0
-    if n==1:
-        return 1
-    return fib(n-1)+fib(n-2)
 
-print(fib(7))
+
+
+
+# # Recursion
+# def fib(n): # 0 1 1 2 3 5 8 13 21 
+#     if n==1:
+#         return 0
+#     if n==2:
+#         return 1
+#     return fib(n-1) + fib(n-2)
+    
+# fib(9)
