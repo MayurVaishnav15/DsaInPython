9108229c1535454408a1abc4130c86ddc29fe371
Mayur Vaishnav <mayurvaishnav1523@gmail.com>
2026-07-31 17:18:08 +0530

    selection_sort

--- Sorting/selection.py
+++ Sorting/selection.py
@@ -15,15 +15,27 @@
 
 
 # Selection Sort Descending Order
-def selectionSort(nums:list):
-    n=len(nums)
-    for i in range(0,n):
-        max_index=i
-        for j in range (i+1,n):
-            if(nums[j]>nums[max_index]):
-                max_index=j 
+# def selectionSort(nums:list):
+#     n=len(nums)
+#     for i in range(0,n):
+#         max_index=i
+#         for j in range (i+1,n):
+#             if(nums[j]>nums[max_index]):
+#                 max_index=j 
 
-        nums[i],nums[max_index]=nums[max_index],nums[i]
-    return nums
+#         nums[i],nums[max_index]=nums[max_index],nums[i]
+#     return nums
 
-print(selectionSort([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+# print(selectionSort([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+# def selectionSort(input:list): # 9,8,7,6,5,4,3,2,1
+#     n = len(input)
+#     for i in range (0,n):
+#         max_index = i
+#         for j in range(i+1,n):
+#             if input[j]>input[i]:
+#                 max_index=j
+#         input[i],input[max_index]=input[max_index],input[i]
+
+#     return input
+# print(selectionSort([1, 2, 3, 4, 5, 6, 7, 8]))
